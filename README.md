f078cbe2fba0358b377be63c7117b7d3a464ef57
pederbl <pederbl@gmail.com>
2022-04-26 19:26:01 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-zejkqp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-zejkqp)
---
+++ index.js
@@ -0,0 +1,4 @@
+// run `node index.js` in the terminal
+import 'dotenv/config';
+
+console.log(process.env);
